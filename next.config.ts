ad4ae965854062234f05ddd25f58400513fdf94d
Antonio Brandao <antoniobrandaodesign@gmail.com>
2024-12-06 11:19:46 +0000

    Edit next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   basePath: "/quantalpha-ai.github.io",
   output: "export",
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
